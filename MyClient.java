ea4b80d0a97c9f29eadd5be3ddf6890fdde3e8e0
tallat97 <tallat.tahir@students.mq.edu.au>
2021-05-29 01:10:49 +1000

    new updated myclient
    I have added new functions into Myclient

--- MyClient.java
+++ MyClient.java
@@ -1,16 +1,21 @@
 import java.io.*;
 import java.net.*;
 import java.nio.charset.StandardCharsets;
-public class MyClient {{
+public class MyClient {
+
+	static DataInputStream din;
+	static PrintStream dout;
+	static Socket s;
+	
 	static String response;
 	static int nRec = 0, nLen = 0;
 	
 	static String largest=" ";
 	
 	public static void main(String[] args) throws Exception {
-		Socket s = new Socket("localhost", 50000);
-		DataInputStream din = new DataInputStream(s.getInputStream());
-		PrintStream dout = new PrintStream(s.getOutputStream(), true);
+		s = new Socket("localhost", 50000);
+		din = new DataInputStream(s.getInputStream());
+		dout = new PrintStream(s.getOutputStream(), true);
 		send("HELO");
 		send("AUTH " + System.getProperty("user.name"));
 		send("REDY");
